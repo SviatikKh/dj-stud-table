21ab5e9510ab4bd56423454e2f540ce7f738b819
SviatikKh <sviatoslavhr@gmail.com>
2021-11-18 16:27:38 +0200

    Add tests

--- statement/teacher/models.py
+++ statement/teacher/models.py
@@ -1,7 +1,6 @@
 import logging
-from django.db import models
-
-logger = logging.getLogger(__name__)
+from django.db import models, IntegrityError, DataError
+Teacherr = logging.getLogger(__name__)
 
 
 class Teacher(models.Model):
@@ -34,3 +33,103 @@ class Teacher(models.Model):
         :return: class, id
         """
         return f'{self.__class__.__name__}(id={self.id})'
+
+    @staticmethod
+    def get_by_id(teacher_id):
+        """
+        :param teacher_id: SERIAL: the id of a Teacher to be found in the DB
+        :return: teacher object or None if a user with such ID does not exist
+        """
+        try:
+            user = Teacher.objects.get(id=teacher_id)
+            return user
+        except Teacher.DoesNotExist:
+            pass
+
+    @staticmethod
+    def delete_by_id(teacher_id):
+        """
+        :param teacher_id: an id of a teacher to be deleted
+        :type teacher_id: int
+        :return: True if object existed in the db and was removed or False if it didn't exist
+        """
+
+        try:
+            teacher = Teacher.objects.get(id=teacher_id)
+            teacher.delete()
+            return True
+        except Teacher.DoesNotExist:
+            pass
+        return False
+
+    @staticmethod
+    def create(name, surname, patronymic):
+        """
+        param name: Describes name of the teacher
+        type name: str max_length=20
+        param surname: Describes surname of the teacher
+        type surname: str max_length=20
+        param patronymic: Describes patronymic of the teacher
+        type patronymic: str max_length=20
+        :return: a new teacher object which is also written into the DB
+        """
+        teacher = Teacher(name=name, surname=surname, patronymic=patronymic)
+        try:
+            teacher.save()
+            return teacher
+        except (IntegrityError, AttributeError, DataError):
+            pass
+
+    @staticmethod
+    def create(name, surname, patronymic):
+        """
+        param name: Describes name of the teacher
+        type name: str max_length=20
+        param surname: Describes surname of the teacher
+        type surname: str max_length=20
+        param patronymic: Describes patronymic of the teacher
+        type patronymic: str max_length=20
+        :return: a new teacher object which is also written into the DB
+        """
+        teacher = Teacher(name=name, surname=surname, patronymic=patronymic)
+        try:
+            teacher.save()
+            return teacher
+        except (IntegrityError, AttributeError, DataError):
+            pass
+
+    def update(self,
+               name=None,
+               surname=None,
+               patronymic=None):
+        """
+        Updates teacher in the database with the specified parameters.\n
+        param name: Describes name of the teacher
+        type name: str max_length=20
+        param surname: Describes surname of the teacher
+        type surname: str max_length=20
+        param patronymic: Describes patronymic of the teacher
+        type patronymic: str max_length=20
+        :return: None
+        """
+
+        if name:
+            self.name = name
+        if surname:
+            self.surname = surname
+        if patronymic:
+            self.patronymic = patronymic
+        try:
+            from django.db import transaction
+            with transaction.atomic():
+                self.save()
+        except:
+            pass
+
+    @staticmethod
+    def get_all():
+        """
+        returns data for json request with QuerySet of all teachers
+        """
+        all_teachers = Teacher.objects.all()
+        return all_teachers
--- statement/teacher/tests.py
+++ statement/teacher/tests.py
@@ -1,3 +1,105 @@
+import datetime
+from unittest import mock
+
+import pytz
 from django.test import TestCase
 
-# Create your tests here.
+from authentication.models import CustomUser
+from .models import Teacher
+
+TEST_DATE = datetime.datetime(2021, 10, 10, 12, 00, tzinfo=pytz.utc)
+TEST_DATE_END = TEST_DATE + datetime.timedelta(days=15)
+
+
+class TestTeacherrModel(TestCase):
+    """Class for CustomUser Model test"""
+
+    def setUp(self):
+        """ Create a user object to be used by the tests """
+        time_mock = datetime.datetime(2021, 10, 10, 12, 00, tzinfo=pytz.utc)
+        with mock.patch('django.utils.timezone.now') as mock_time:
+            mock_time.return_value = time_mock
+            self.user = CustomUser(id=111, email='email@mail.com', password='1234', first_name='fname',
+                                   middle_name='mname',
+                                   last_name='lname')
+            self.user.save()
+            self.user_free = CustomUser(id=222, email='2email@mail.com', password='1234', first_name='2fname',
+                                        middle_name='2mname',
+                                        last_name='2lname')
+            self.user_free.save()
+
+            self.teacher1 = Teacher(id=101, name="teacher1", surname="s1", patronymic="p1")
+            self.teacher1.save()
+
+            self.teacher2 = Teacher(id=102, name="teacher2", surname="s2", patronymic="p2")
+            self.teacher2.save()
+
+    def test_get_by_id_positive(self):
+        """Positive test of the CustomUser.get_by_id() method"""
+        teacher = Teacher.get_by_id(101)
+        self.assertEqual(teacher.id, 101)
+        self.assertEqual(teacher.name, 'teacher1')
+        self.assertEqual(teacher.surname, "s1")
+        self.assertEqual(teacher.patronymic, "p1")
+
+    def test_get_by_id_negative(self):
+        """Negative test of the CustomUser.get_by_id() method"""
+        teacher = Teacher.get_by_id(999)
+        self.assertIsNone(teacher)
+
+    def test_delete_by_id_positive(self):
+        """ Test of the CustomUser.delete_by_id() method """
+        self.assertTrue(Teacher.delete_by_id(101))
+        self.assertRaises(Teacher.DoesNotExist, Teacher.objects.get, pk=101)
+
+    def test_delete_by_id_negative(self):
+        """ Test of the CustomUser.delete_by_id() method """
+        self.assertFalse(Teacher.delete_by_id(999))
+
+    def test_get_all(self):
+        """ Positive Test of the CustomUser.create method TEST_DATE_END"""
+        teachers = list(Teacher.get_all())
+        teachers.sort(key=lambda teacher: teacher.id)
+        self.assertListEqual(teachers, [self.teacher1, self.teacher2])
+
+    def test_update(self):
+        teacher = Teacher.objects.get(id=101)
+        teacher.update(name="testName", surname="testSurname", patronymic="testPatronymic")
+
+        teacher = Teacher.objects.get(id=101)
+        self.assertIsInstance(teacher, Teacher)
+        self.assertEqual(teacher.name, "testName")
+        self.assertEqual(teacher.surname, "testSurname")
+        self.assertEqual(teacher.patronymic, "testPatronymic")
+
+    def test_update_only_name(self):
+        teacher = Teacher.objects.get(id=101)
+        teacher.update(name="testName")
+
+        teacher = Teacher.objects.get(id=101)
+        self.assertIsInstance(teacher, Teacher)
+        self.assertEqual(teacher.name, "testName")
+        self.assertEqual(teacher.surname, "s1")
+        self.assertEqual(teacher.patronymic, "p1")
+
+    def test_update_not_valid_name(self):
+        teacher = Teacher.objects.get(id=101)
+        teacher.update(name="testName" * 5)
+
+        teacher = Teacher.objects.get(id=101)
+        self.assertIsInstance(teacher, Teacher)
+        self.assertEqual(teacher.name, "teacher1")
+        self.assertEqual(teacher.surname, "s1")
+        self.assertEqual(teacher.patronymic, "p1")
+
+    def test_create(self):
+        teacher = Teacher.create(name="testName", surname="s1", patronymic="p1")
+        teacher = Teacher.objects.get(id=teacher.id)
+        self.assertIsInstance(teacher, Teacher)
+        self.assertEqual(teacher.name, "testName")
+        self.assertEqual(teacher.surname, "s1")
+        self.assertEqual(teacher.patronymic, "p1")
+
+    def test_createnot_valid_surname(self):
+        teacher = Teacher.create(name="testName", surname="s1" * 20, patronymic="p1")
+        self.assertIsNone(teacher)
